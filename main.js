abc321e5a215d21b9ea50274bfd48d3b050f3c10
maryamlatif <161033266+maryamlatif@users.noreply.github.com>
2024-03-20 00:29:22 +0500

    class 2
    Difference between "let","var" ,"const".

---
+++ main.js
@@ -0,0 +1,18 @@
+//variable decleared
+var myname;
+myname = "maham";
+// console.log(myname);
+// This is not print cause of top to bottom print
+myname = "iqra";
+var $Name = "hani";
+//console.log($Name);
+var age = "29";
+//console.log(age);
+var Fnam = "mahaa";
+var Age;
+Age = 24;
+var responsible;
+responsible = true;
+var height = 5.3;
+var student = "iqra";
+var student = "saba";
---
+++ main.ts
@@ -0,0 +1,40 @@
+
+
+//variable decleared
+
+let myname ;
+
+
+myname = "maham";
+
+// console.log(myname);
+
+// This is not print cause of top to bottom print
+
+myname = "iqra";
+
+
+let $Name = "hani";
+
+
+//console.log($Name);
+
+
+
+const age = "29";
+
+//console.log(age);
+
+
+let Fnam:string = "mahaa"
+let Age:number;
+Age = 24 ;
+let responsible:boolean;
+responsible = true
+const height:number = 5.3
+
+//var has lost of errors we cant use "var" we only use "let "or "const"
+
+var student ="iqra"
+var student = "saba"
+
